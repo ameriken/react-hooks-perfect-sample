f3cab717f48907bb4b67b04168c40e0658960b23
ameriken <hitohahito@hotmail.co.jp>
2020-06-23 00:53:58 +0900

    useStateを学ぶ

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CounterHooks from "./component/CounterHooks";
+import FormHook from "./component/FormHook";
+import ItemHook from "./component/ItemHook";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*<CounterHooks /> */}
+      {/*<FormHook/> */}
+      <ItemHook/>
     </div>
   );
 }
