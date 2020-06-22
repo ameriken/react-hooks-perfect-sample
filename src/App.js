e0c77b6b0adaf2e028b64e2095c3e7ab366b9af3
ameriken <hitohahito@hotmail.co.jp>
2020-06-23 02:26:53 +0900

    useContextを学ぶ

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {createContext, useState} from 'react';
 import './App.css';
 import CounterHooks from "./component/CounterHooks";
 import FormHook from "./component/FormHook";
@@ -7,17 +7,28 @@ import EffectHook from "./component/EffectHook";
 import MouseEventEffect from "./component/MouseEventEffect";
 import DataFetch from "./component/DataFetch";
 import DataFetchById from "./component/DataFetchById";
+import ComponentC from "./component/ComponentC";
+
+export const UserContext = createContext()
+export const LanguageContext = createContext()
 
 function App() {
+    const [user, setUser] = useState({name: 'yamada', age:'32'})
+    const [language, setLanguage] = useState('日本語')
   return (
     <div className="App">
-      {/*<CounterHooks />
-      {/*<FormHook/>
-      <ItemHook/>
-      <EffectHook />
-      <MouseEventEffect/>
-      <DataFetch /> */}
-      <DataFetchById />
+        {/*<CounterHooks />
+            <FormHook/>
+            <ItemHook/>
+            <EffectHook />
+            <MouseEventEffect/>
+            <DataFetch />
+            <DataFetchById /> */}
+        <UserContext.Provider value={user}>
+            <LanguageContext.Provider value={language}>
+                <ComponentC />
+            </LanguageContext.Provider>
+        </UserContext.Provider>
     </div>
   );
 }
---
+++ src/component/ComponentE.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ComponentF from "./ComponentF";
+
+function ComponentE(props) {
+    return (
+        <div>
+            <ComponentF />
+
+        </div>
+    );
+}
+
+export default ComponentE;
